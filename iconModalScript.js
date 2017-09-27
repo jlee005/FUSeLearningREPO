651120ddf5f6f6f8559dd95082f156d90bdd1cf8
Dfay002 <dfay002@student.franciscan.edu>
2017-09-27 16:36:41 -0400

    Add files via upload

---
+++ iconModalScript.js
@@ -0,0 +1,283 @@
+
+<script>jQuery.noConflict();</script>
+<script type="text/javascript">
+    Event.observe(document,"dom:loaded", function() {
+        </script>
+        <script>
+
+
+            var modal1 = document.getElementById("academicSupportModal");
+            var btn1 = document.getElementById("fusdiv1");
+            var span1 = document.getElementsByClassName("close")[0];
+            
+            if (document.body.contains(btn1)){
+                btn1.onclick = function(){
+                    modal1.style.display = "block";
+                }   
+            }
+            
+            span1.onclick = function(){
+                modal1.style.display = "none";
+            }
+            
+            
+            
+            
+            var modal2 = document.getElementById("announcementsModal");
+            var btn2 = document.getElementById("fusdiv2");
+            var span2 = document.getElementsByClassName("close")[1];
+        
+             if (document.body.contains(btn2)){
+                btn2.onclick = function(){
+                    modal2.style.display = "block";
+                }   
+            }
+        
+            span2.onclick = function(){
+                modal2.style.display = "none";
+            }
+            
+            
+        
+            var modal3 = document.getElementById("discussionsModal");
+            var btn3 = document.getElementById("fusdiv3");
+            var span3 = document.getElementsByClassName("close")[2];
+        
+             if (document.body.contains(btn3)){
+                btn3.onclick = function(){
+                    modal3.style.display = "block";
+                }   
+            }
+        
+            span3.onclick = function(){
+                modal3.style.display = "none";
+            }
+            
+            
+        
+            var modal4 = document.getElementById("emailModal");
+            var btn4 = document.getElementById("fusdiv4");
+            var span4 = document.getElementsByClassName("close")[3];
+        
+            if (document.body.contains(btn4)){
+                btn4.onclick = function(){
+                    modal4.style.display = "block";
+                }   
+            }
+        
+            span4.onclick = function(){
+                modal4.style.display = "none";
+            }
+        
+                
+        
+        
+            var modal5 = document.getElementById("learningSessionsModal");
+            var btn5 = document.getElementById("fusdiv5");
+            var span5 = document.getElementsByClassName("close")[4];
+        
+             if (document.body.contains(btn5)){
+                btn5.onclick = function(){
+                    modal5.style.display = "block";
+                }   
+            }
+        
+            span5.onclick = function(){
+                modal5.style.display = "none";
+            }
+        
+            
+        
+            var modal6 = document.getElementById("facultyModal");
+            var btn6 = document.getElementById("fusdiv6");
+            var span6 = document.getElementsByClassName("close")[5];
+        
+             if (document.body.contains(btn6)){
+                btn6.onclick = function(){
+                    modal6.style.display = "block";
+                }   
+            }
+        
+            span6.onclick = function(){
+                modal6.style.display = "none";
+            }
+        
+            
+        
+        
+            var modal7 = document.getElementById("gradebookModal");
+            var btn7 = document.getElementById("fusdiv7");
+            var span7 = document.getElementsByClassName("close")[6];
+        
+             if (document.body.contains(btn7)){
+                btn7.onclick = function(){
+                    modal7.style.display = "block";
+                }   
+            }
+        
+            span7.onclick = function(){
+                modal7.style.display = "none";
+            }
+        
+            
+        
+            var modal8 = document.getElementById("homeModal");
+            var btn8 = document.getElementById("fusdiv8");
+            var span8 = document.getElementsByClassName("close")[7];
+        
+             if (document.body.contains(btn8)){
+                btn8.onclick = function(){
+                    modal8.style.display = "block";
+                }   
+            }
+        
+            span8.onclick = function(){
+                modal8.style.display = "none";
+            }
+        
+            
+        
+        
+            var modal9 = document.getElementById("supportModal");
+            var btn9 = document.getElementById("fusdiv9");
+            var span9 = document.getElementsByClassName("close")[8];
+        
+             if (document.body.contains(btn9)){
+                btn9.onclick = function(){
+                    modal9.style.display = "block";
+                }   
+            }
+        
+            span9.onclick = function(){
+                modal9.style.display = "none";
+            }
+            
+            
+            
+            var modal10 = document.getElementById("papersModal");
+            var btn10 = document.getElementById("fusdiv10");
+            var span10 = document.getElementsByClassName("close")[9];
+        
+             if (document.body.contains(btn10)){
+                btn10.onclick = function(){
+                    modal10.style.display = "block";
+                }   
+            }
+        
+            span10.onclick = function(){
+                modal10.style.display = "none";
+            }
+            
+            
+
+            var modal11 = document.getElementById("projectsModal");
+            var btn11 = document.getElementById("fusdiv11");
+            var span11 = document.getElementsByClassName("close")[10];
+        
+            if (document.body.contains(btn11)){
+                btn11.onclick = function(){
+                    modal11.style.display = "block";
+                }   
+            }
+        
+            span11.onclick = function(){
+                modal11.style.display = "none";
+            }
+            
+            
+
+            var modal12 = document.getElementById("examsModal");
+            var btn12 = document.getElementById("fusdiv12");
+            var span12 = document.getElementsByClassName("close")[11];
+        
+             if (document.body.contains(btn12)){
+                btn12.onclick = function(){
+                    modal12.style.display = "block";
+                }   
+            }
+        
+            span12.onclick = function(){
+                modal12.style.display = "none";
+            }
+            
+            
+
+            var modal13 = document.getElementById("blogsModal");
+            var btn13 = document.getElementById("fusdiv13");
+            var span13 = document.getElementsByClassName("close")[12];
+        
+             if (document.body.contains(btn13)){
+                btn13.onclick = function(){
+                    modal13.style.display = "block";
+                }   
+            }
+        
+            span13.onclick = function(){
+                modal13.style.display = "none";
+            }
+
+
+
+            var modal14 = document.getElementById("journalsModal");
+            var btn14 = document.getElementById("fusdiv14");
+            var span14 = document.getElementsByClassName("close")[13];
+        
+             if (document.body.contains(btn14)){
+                btn14.onclick = function(){
+                    modal14.style.display = "block";
+                }   
+            }
+        
+            span14.onclick = function(){
+                modal14.style.display = "none";
+            }
+
+
+
+            window.onclick = function(event){
+                if (modal1.style.display == "block" && event.target == modal1){
+                    modal1.style.display = "none";
+                }
+                if (modal2.style.display == "block" && event.target == modal2){
+                    modal2.style.display = "none";
+                }
+                if (modal3.style.display == "block" && event.target == modal3){
+                    modal3.style.display = "none";
+                }
+                if (modal4.style.display == "block" && event.target == modal4){
+                    modal4.style.display = "none";
+                }
+                if (modal5.style.display == "block" && event.target == modal5){
+                    modal5.style.display = "none";
+                }
+                if (modal6.style.display == "block" && event.target == modal6){
+                    modal6.style.display = "none";
+                }
+                if (modal7.style.display == "block" && event.target == modal7){
+                    modal7.style.display = "none";
+                }
+                if (modal8.style.display == "block" && event.target == modal8){
+                    modal8.style.display = "none";
+                }
+                if (modal9.style.display == "block" && event.target == modal9){
+                    modal9.style.display = "none";
+                }
+                if (modal10.style.display == "block" && event.target == modal10){
+                    modal10.style.display = "none";
+                }
+                if (modal11.style.display == "block" && event.target == modal11){
+                    modal11.style.display = "none";
+                }
+                if (modal12.style.display == "block" && event.target == modal12){
+                    modal12.style.display = "none";
+                }
+                if (modal13.style.display == "block" && event.target == modal13){
+                    modal13.style.display = "none";
+                }
+                if (modal14.style.display == "block" && event.target == modal14){
+                    modal14.style.display = "none";
+                }                
+            }
+        </script>
+    
+
